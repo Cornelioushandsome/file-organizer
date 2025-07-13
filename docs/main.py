426ef17a9b532b162aacd2fd76d5faebb06a8209
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2025-07-13 16:20:02 -0400

    Update main.py

--- docs/main.py
+++ docs/main.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 from pathlib import Path
 import shutil
+from datetime import datetime
 #import colorama
 class FileExtensions:
     videoExtensions = [
@@ -27,6 +28,22 @@ class FileExtensions:
     ".exe", ".bat", ".cmd", ".msi", ".app", ".deb"
     ]
 
+def _checkFile(path: Path):
+    if not path.exists():
+        return False
+    if not path.is_file():
+        return False
+    return True
+
+def _checkFolder(path: Path):
+    if not path.exists():
+        print("Path does not exist")
+        return False
+    if not path.is_dir():
+        print("Path is not a directory")
+        return False
+    return True
+
 def getFileType(extension)->str:
     types = FileExtensions()
     if extension in types.archiveExtensions:
@@ -47,7 +64,7 @@ def getFileType(extension)->str:
 
 def reorganize(fileList):
     for file in fileList:
-        name, ext = os.path.splitext(file)
+        _, ext = os.path.splitext(file)
         fileType = getFileType(ext)
         if fileType =="unknown":
             print("Unknown file type; Skipping")
@@ -65,10 +82,9 @@ def reorganize(fileList):
         print(f"Moved: {file} to {destination}")
 
 def deleteFile(filePath: Path):
-    if not filePath.exists():
-        raise NameError("FolderPath does not exist")
-    if not filePath.is_file():
-        raise NameError("FolderPath is not a directory")
+    filePath = Path(filePath)
+    if not _checkFile(filePath):
+        raise NameError("Invalid filePath")
     
     try:
         filePath.unlink()
@@ -79,10 +95,10 @@ def deleteFile(filePath: Path):
         return
 
 def deleteFolder(folderPath: Path):
-    if not folderPath.exists():
-        raise NameError("FolderPath does not exist")
-    if not folderPath.is_dir():
-        raise NameError("FolderPath is not a directory")
+    folderPath = Path(folderPath)
+    if not _checkFolder(folderPath):
+        raise NameError("Invalid folder path")
+
     
     if folderPath.stat().st_size > 0:
         confirm = input(f"Folder: \"{folderPath}\" is not empty. Would you still like to delete anyway? [Y : N]")
@@ -97,10 +113,83 @@ def deleteFolder(folderPath: Path):
     except Exception as e:
         print(f"Failed to delete file: {e}")
         return
+
+def unorganize(folderPath: Path):
+    folderPath = Path(folderPath)
+
+    if not folderPath.is_dir():
+        print("Folder Path is not a directory")
+        return
+    if not folderPath.exists():
+        print("Folder Path does not exist")
+        return
     
-def main():
+    for dirs, _, files in os.walk(folderPath, topdown=False): # folder path: Path  |   folders in path: list  |    files in the folder: list
+        for file in files:
+            file_path = Path(dirs) / file
+            destination_path = folderPath / file
+            
+            if destination_path.exists():
+                counter = 1
+                new_destination = destination_path.with_stem(destination_path.stem + f"_{counter}")
+                while new_destination.exists():
+                    counter+=1
+                    new_destination = destination_path.with_stem(destination_path.stem + f"_{counter}")
+                destination_path = new_destination
+            
+            shutil.move(str(file_path), str(destination_path))
+            print(f"Moved {file_path} to {destination_path} \n")
+
+        if Path(dirs) != folderPath:
+            try:
+                os.rmdir(dirs)
+                print(f"Removed directory: {dirs}")
+            except Exception as e:
+                print(f"Error removing subdirectory: {dirs} | Error: {e}")
+                return
+
+def rename(path: Path, newName: str):
+    path = Path(path)
+
+    if not path.exists():
+        print(f"Path: {path} doesn't exist or was already renamed.")
+        return
+    
+    old_path = path
+    new_path = path.with_name((newName + path.suffix) if path.is_file() else newName)
+    if new_path.exists():
+        print("Path already exists")
+        return
+    try:
+        path.rename(new_path)
+        print(f"Successfully renamed {old_path} to {new_path}")
+    except Exception as e:
+        print(f"An error occured while renaming {old_path} | Error: {e}")
+        return   
+
+def backupFolder(folderPath: Path, root: Path=None):
+    folderPath = Path(folderPath)
+    if not _checkFolder(folderPath):
+        raise NameError("Invalid folder path")  
+
+    backupFolder = Path(root) if root else folderPath.parent/"backups"
+    backupFolder.mkdir(exist_ok=True)
+
+    time = datetime.now().strftime(r"%Y-%m-%d")
+    backupPath = backupFolder / f"{backupFolder.name}_backup-{time}"
+    try:
+        shutil.copytree(folderPath, backupPath)
+        print(f"Successfully backed up {folderPath} to {backupPath}")
+        return
+    except Exception as e:
+        print(f"An error occured: {e}")
+        return
+    
+def main():  #enter your code here
     pass
-        
+
     
 if __name__ == "__main__":
+    os.chdir(r"")  #Enter directory here
+    PATH = os.getcwd()
     main()
